811c3abb56b2be3e4e3b707d76e722219a1a3e0e
Max Holt <max.holt1@gmail.com>
2024-09-15 16:29:40 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
 /*
 *
 Option #1: Get Weekly Temperatures
@@ -13,7 +18,53 @@ Compile and submit your pseudocode, source code, and screenshots of the applicat
 
 * */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Double> temperatures = new ArrayList<Double>(Arrays.asList(90.2, 93.2, 91.5, 90.3, 89.3, 85.9, 87.8));
+        ArrayList<String> weekDays = new ArrayList<String>(Arrays.asList(
+                "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
+        ));
+
+        System.out.println("Enter the day of the week to see it's average temperature: ");
+        Scanner userInput = new Scanner(System.in);
+        String userChoice = userInput.nextLine();
+
+        Double temp = null;
+        switch (userChoice.toLowerCase()) {
+            case "sunday":
+                temp = temperatures.get(0);
+                break;
+            case "monday":
+                temp = temperatures.get(1);
+                break;
+            case "tuesday":
+                temp = temperatures.get(2);
+                break;
+            case "wednesday":
+                temp = temperatures.get(3);
+                break;
+            case "thursday":
+                temp = temperatures.get(4);
+                break;
+            case "friday":
+                temp = temperatures.get(5);
+                break;
+            case "saturday":
+                temp = temperatures.get(6);
+                break;
+            case "week":
+                temp = null;
+                break;
+            default:
+                temp = null;
+        }
+        
+        if (temp == null) {
+            for (int i = 0; i < temperatures.size(); i++) {
+                System.out.println(weekDays.get(i) + " : " + temperatures.get(i));
+            }
+        } else {
+            System.out.println("Temperature for " + userChoice.toUpperCase() + " : " + temp);
+        }
     }
 }
